1d003da5b2d683db560f1ae2a262327dc4bb8a2e
vijay212003 <vtu22660@veltech.edu.in>
2025-09-22 16:00:58 +0530

    Create task2a.java

---
+++ TASK1/task2a.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+class Solution {
+    public int[][] intervalIntersection(int[][] firstList, int[][] secondList) {
+        List<int[]> result = new ArrayList<>();
+        int i = 0, j = 0;
+
+        while (i < firstList.length && j < secondList.length) {
+            int start = Math.max(firstList[i][0], secondList[j][0]);
+            int end = Math.min(firstList[i][1], secondList[j][1]);
+
+            if (start <= end) {
+                result.add(new int[]{start, end});
+            }
+
+            if (firstList[i][1] < secondList[j][1]) {
+                i++;
+            } else {
+                j++;
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[][] firstList = {{0,2},{5,10},{13,23},{24,25}};
+        int[][] secondList = {{1,5},{8,12},{15,24},{25,26}};
+        int[][] intersections = sol.intervalIntersection(firstList, secondList);
+        System.out.println("Intersections: " + Arrays.deepToString(intersections));
+    }
+}
